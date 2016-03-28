acfc770e70bb12b0073f5aae9bde4e7f09b4ca43
Hornwitser <github@hornwitser.no>
2016-03-28 03:59:48 +0000

    Add require.js
    Use require.js to turn the existing codebase into a few independent
    modules for better organization of the codebase.  This removes all
    globals except for togglePause, and toggleCleanBlit.

--- app/main.js
+++ app/main.js
@@ -1,248 +1,249 @@
 "use strict";
+define(["physics", "particle"], function (physics, particle) {
+	var Particle = particle.Particle;
 
-var page = {
-	canvas: null,
-	dPanel: null,
-	game: {
-		timeLine: []
+	var page = {
+		canvas: null,
+		dPanel: null,
+		game: {
+			timeLine: []
+		}
+	};
+
+	var currentRealTime = 0;
+	var lastRealTime = 0;
+	var deltaRealTime = 0;
+	var currentGameTime = 0;
+	var dt = 0; //Delta Game Time
+
+	//Returns an array filled with count items of the specified value.
+	function filledArray(count, value) {
+		var array = [];
+		for (var i=0; i<count; i++) {
+			array[i] = value;
+		}
+		return array;
 	}
-};
 
-var currentRealTime = 0;
-var lastRealTime = 0;
-var deltaRealTime = 0;
-var currentGameTime = 0;
-var dt = 0; //Delta Game Time
+	//These arrays are used for graphing the framerate.
+	var dtHistory = filledArray(50, 0);
+	var dRTHistory = filledArray(50, 0);
 
-//Returns an array filled with count items of the specified value.
-function filledArray(count, value) {
-	var array = [];
-	for (var i=0; i<count; i++) {
-		array[i] = value;
+	//HTML Buttons and Stuff
+	var paused = false;
+	var cleanBlit = true;
+	window.togglePause = function (){ paused = !paused; };
+	window.toggleCleanBlit = function (){ cleanBlit = !cleanBlit; };
+
+	function event(time, func, para, desc, timeDependant){
+		if ( !(this instanceof event) ) throw new Error("Constructor called as a function");
+		this.time = time;
+		this.func = func;
+		this.para = para;
+		this.desc = desc;
+		this.timeDependant = timeDependant;
+		this.timeSpent = 0;
+		this.result = undefined;
 	}
-	return array;
-}
 
-//These arrays are used for graphing the framerate.
-var dtHistory = filledArray(50, 0);
-var dRTHistory = filledArray(50, 0);
-
-//HTML Buttons and Stuff
-var paused = false;
-var cleanBlit = true;
-function togglePause(){ paused = !paused; }
-function toggleCleanBlit(){ cleanBlit = !cleanBlit; }
-
-function event(time, func, para, desc, timeDependant){
-	if ( !(this instanceof event) ) throw new Error("Constructor called as a function");
-	this.time = time;
-	this.func = func;
-	this.para = para;
-	this.desc = desc;
-	this.timeDependant = timeDependant;
-	this.timeSpent = 0;
-	this.result = undefined;
-}
-
-function checkTimeLine(pTime){
-	//make sure it's an event we're looking at, else purge it till we arrive to an event or exhause the timeline
-	while( !(page.game.timeLine[0] instanceof event) && page.game.timeLine.length > 0){
-		var junk = page.game.timeLine.shift();
-		debugger;
+	function checkTimeLine(pTime){
+		//make sure it's an event we're looking at, else purge it till we arrive to an event or exhause the timeline
+		while( !(page.game.timeLine[0] instanceof event) && page.game.timeLine.length > 0){
+			var junk = page.game.timeLine.shift();
+			debugger;
+		}
+		if (page.game.timeLine.length > 0){
+			if (pTime >= page.game.timeLine[0].time) {
+				return true;
+			}
+		}
+		return false;
 	}
-	if (page.game.timeLine.length > 0){
-		if (pTime >= page.game.timeLine[0].time) {
-			return true;
+
+	function nextFrame(){
+		//What time is it, and how much time has passed?
+		lastRealTime = currentRealTime;
+		currentRealTime = new Date().getTime();
+		deltaRealTime = currentRealTime - lastRealTime;
+
+		if (deltaRealTime > 1000 || paused){ //If over a second has passed, assume the game has been paused.
+			dt = 0;
+		}else{
+			//Have we arrived at any timeline events?  If so, increment time to exactly the trigger time of the next event.
+			if(checkTimeLine(currentGameTime + deltaRealTime)){ dt = page.game.timeLine[0].time - currentGameTime;
+				}else{ 											dt = deltaRealTime; }
+
+			currentGameTime += dt;
+		}
+
+		//Update our graphs.
+		dtHistory.unshift(dt);
+		dtHistory.pop();
+		dRTHistory.unshift(deltaRealTime);
+		dRTHistory.pop();
+
+		//Execute any events
+		while(checkTimeLine(currentGameTime)){
+			var currentEvent = page.game.timeLine.shift();
+			currentEvent.func(currentEvent.para);
+		}
+
+		physics.integrate(page.game.test, currentGameTime, dt / 1000);
+
+		page.game.ball.tick(dt);
+		update();
+		draw();
+		requestAnimationFrame(nextFrame);
+	}
+
+	//This function is called by the event handler when the event is a collision.  It handles the collision and detects/enqueues the next collision in the system.
+	function collision(hitObject){
+
+		//Have the collision take effect on the ball.
+		if (hitObject != undefined){
+			var x = page.game.ball.getXVel();
+			var y = page.game.ball.getYVel();
+
+			//Only the ball and the walls are colliable objects for the time being.
+			if (hitObject == "left wall" || hitObject == "right wall") x = page.game.ball.getXVel() * -1;
+			if (hitObject == "top wall" || hitObject == "bottom wall") y = page.game.ball.getYVel() * -1;
+			page.game.ball.setVel(x, y);
+		}
+
+		//find the next collision, if we're moving.
+		if (page.game.ball.getXVel() != 0 || page.game.ball.getYVel() != 0){
+			var dx = (page.game.ball.getXVel() < 0) ? page.game.ball.getXPos() : 600 - page.game.ball.getXPos();
+			var dxWall = (page.game.ball.getXVel() < 0) ? "left wall" : "right wall";
+			var dy = (page.game.ball.getYVel() < 0) ? page.game.ball.getYPos() : 400 - page.game.ball.getYPos();
+			var dyWall = (page.game.ball.getYVel() < 0) ? "top wall" : "bottom wall";
+			var tx = Math.round(dx/Math.abs(page.game.ball.getXVel()));
+			var ty = Math.round(dy/Math.abs(page.game.ball.getYVel()));
+			var t = (tx < ty) ? tx : ty;
+			var hitWall = (tx < ty) ? dxWall : dyWall;
+
+			if (t > 0){
+				var nextCollision = new event(t + currentGameTime, collision, hitWall, "collision", false);
+				page.game.timeLine.push(nextCollision);
+				page.game.timeLine.sort(function (a, b){ return a.time - b.time; });
+			}
 		}
 	}
-	return false;
-}
 
-function nextFrame(){
-	//What time is it, and how much time has passed?
-	lastRealTime = currentRealTime;
-	currentRealTime = new Date().getTime();
-	deltaRealTime = currentRealTime - lastRealTime;
+	function update(){
 
-	if (deltaRealTime > 1000 || paused){ //If over a second has passed, assume the game has been paused.
-		dt = 0;
-	}else{
-		//Have we arrived at any timeline events?  If so, increment time to exactly the trigger time of the next event.
-		if(checkTimeLine(currentGameTime + deltaRealTime)){ dt = page.game.timeLine[0].time - currentGameTime;
-			}else{ 											dt = deltaRealTime; }
+		var m = 0.003;
+		var a = Math.atan2(page.game.ball.getYPos() - page.game.dragon.getYPos(), page.game.ball.getXPos() - page.game.dragon.getXPos());
 
-		currentGameTime += dt;
+		var x = m*Math.cos(a);
+		var y = m*Math.sin(a);
+		page.game.dragon.setAcc(x, y);
+
+		page.game.dragon.tick(dt);
+
 	}
 
-	//Update our graphs.
-	dtHistory.unshift(dt);
-	dtHistory.pop();
-	dRTHistory.unshift(deltaRealTime);
-	dRTHistory.pop();
+	function draw(){
 
-	//Execute any events
-	while(checkTimeLine(currentGameTime)){
-		var currentEvent = page.game.timeLine.shift();
-		currentEvent.func(currentEvent.para);
-	}
+		var ctx = page.canvas.getContext("2d");
+		var dctx = page.dPanel.getContext("2d");
 
-	physics.integrate(page.game.test, currentGameTime, dt / 1000);
-
-	page.game.ball.tick(dt);
-	update();
-	draw();
-	requestAnimationFrame(nextFrame);
-}
-
-//This function is called by the event handler when the event is a collision.  It handles the collision and detects/enqueues the next collision in the system.
-function collision(hitObject){
-
-	//Have the collision take effect on the ball.
-	if (hitObject != undefined){
-		var x = page.game.ball.getXVel();
-		var y = page.game.ball.getYVel();
-
-		//Only the ball and the walls are colliable objects for the time being.
-		if (hitObject == "left wall" || hitObject == "right wall") x = page.game.ball.getXVel() * -1;
-		if (hitObject == "top wall" || hitObject == "bottom wall") y = page.game.ball.getYVel() * -1;
-		page.game.ball.setVel(x, y);
-	}
-
-	//find the next collision, if we're moving.
-	if (page.game.ball.getXVel() != 0 || page.game.ball.getYVel() != 0){
-		var dx = (page.game.ball.getXVel() < 0) ? page.game.ball.getXPos() : 600 - page.game.ball.getXPos();
-		var dxWall = (page.game.ball.getXVel() < 0) ? "left wall" : "right wall";
-		var dy = (page.game.ball.getYVel() < 0) ? page.game.ball.getYPos() : 400 - page.game.ball.getYPos();
-		var dyWall = (page.game.ball.getYVel() < 0) ? "top wall" : "bottom wall";
-		var tx = Math.round(dx/Math.abs(page.game.ball.getXVel()));
-		var ty = Math.round(dy/Math.abs(page.game.ball.getYVel()));
-		var t = (tx < ty) ? tx : ty;
-		var hitWall = (tx < ty) ? dxWall : dyWall;
-
-		if (t > 0){
-			var nextCollision = new event(t + currentGameTime, collision, hitWall, "collision", false);
-			page.game.timeLine.push(nextCollision);
-			page.game.timeLine.sort(function (a, b){ return a.time - b.time; });
+		//Start with a clean plate
+		if (cleanBlit){
+			ctx.clearRect(0, 0, page.canvas.width, page.canvas.height);
 		}
+		dctx.clearRect(0, 0, page.dPanel.width, page.dPanel.height);
+
+		//Draw the FPS chart
+		dctx.beginPath();
+		dctx.strokeStyle = "rgba(255, 0, 0, 0.8)";
+		dctx.moveTo(10, 10);
+		var j = 0;
+		for (var i=0; i<dRTHistory.length; i++){
+			dctx.moveTo(10+i, 50);
+			dctx.lineTo(10+i, 50-(1000/dRTHistory[i])/2);
+			j += dRTHistory[i];
+		}
+		dctx.stroke();
+		dctx.fillText(Math.round(1000/(j/dRTHistory.length)) + "FPS", 20, 40);
+
+		//Draw gametime chart
+		dctx.beginPath();
+		dctx.strokeStyle = "rgba(128, 0, 128, 0.8)";
+		dctx.moveTo(10, 60);
+		j = 0;
+		for (var i=0; i<dtHistory.length; i++){
+			dctx.moveTo(10+i, 100);
+			dctx.lineTo(10+i, 100-dtHistory[i]/2);
+			j += dtHistory[i];
+		}
+		dctx.stroke();
+		dctx.fillText(Math.round(j/dtHistory.length) + "dt", 20, 90);
+
+		//Draw timeline
+		dctx.beginPath(); //Current Time Marker
+		dctx.strokeStyle = "rgba(0, 128, 128, 0.8)";
+		dctx.moveTo(2, 110);
+		dctx.lineTo(50, 110);
+		dctx.stroke();
+		dctx.fillText(currentGameTime + "ms", 70, 110);
+
+		dctx.beginPath(); //Second ticks
+		dctx.strokeStyle = "rgba(0, 0, 0, 0.8)";
+		dctx.moveTo(10, 110);
+		dctx.lineTo(10, 410);
+		for (var i=0; i<29; i++){
+			var y = 120 + (10 * i) - (currentGameTime % 1000)/100;
+			dctx.moveTo(5, y);
+			dctx.lineTo(15, y);
+		}
+		dctx.stroke();
+
+		dctx.beginPath(); //Event ticks
+		dctx.strokeStyle = "rgba(0, 0, 255, 0.8)";
+		for (var i=0; i<page.game.timeLine.length; i++){
+			y = 110 + (page.game.timeLine[i].time-currentGameTime)/100;
+			dctx.moveTo(2, y);
+			dctx.lineTo(25, y);
+			dctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+			dctx.fillText(page.game.timeLine[i].desc + ": " + page.game.timeLine[i].para, 25, y);
+		}
+		dctx.stroke();
+
+		//Draw ball
+		ctx.beginPath();
+		ctx.arc(page.game.ball.getXPos(), page.game.ball.getYPos(), page.game.ball.radius, 0, Math.PI*2, true);
+		ctx.fillStyle = "rgba(0, 128, 175, 0.8)";
+		ctx.fill();
+
+		//Draw physics test thing
+		ctx.beginPath();
+		ctx.arc(page.game.test.x + 200, page.game.test.y + 200, 10, 0, Math.PI*2, true);
+		ctx.fillStyle = "rgba(0, 175, 64, 0.8)";
+		ctx.fill();
+
+		//Draw dragon
+		ctx.beginPath();
+		ctx.arc(page.game.dragon.getXPos(), page.game.dragon.getYPos(), page.game.dragon.radius, 0, Math.PI*2, true);
+		ctx.fillStyle = "rgba(0, 0, 175, 0.8)";
+		ctx.fill();
+
+		//draw grid
+		ctx.lineWidth = 1;
+		ctx.beginPath();
+		ctx.strokeStyle = "rgba(0, 0, 0, 0.1)";
+		for(var x = 0; x < 600; x = x + 10){
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, 400);
+		}
+		for(var y = 10; y < 400; y = y + 10){
+			ctx.moveTo(0, y);
+			ctx.lineTo(600, y);
+		}
+		ctx.stroke();
 	}
-}
 
-function update(){
-
-	var m = 0.003;
-	var a = Math.atan2(page.game.ball.getYPos() - page.game.dragon.getYPos(), page.game.ball.getXPos() - page.game.dragon.getXPos());
-
-	var x = m*Math.cos(a);
-	var y = m*Math.sin(a);
-	page.game.dragon.setAcc(x, y);
-
-	page.game.dragon.tick(dt);
-
-}
-
-function draw(){
-
-	var ctx = page.canvas.getContext("2d");
-	var dctx = page.dPanel.getContext("2d");
-
-	//Start with a clean plate
-	if (cleanBlit){
-		ctx.clearRect(0, 0, page.canvas.width, page.canvas.height);
-	}
-	dctx.clearRect(0, 0, page.dPanel.width, page.dPanel.height);
-
-	//Draw the FPS chart
-	dctx.beginPath();
-	dctx.strokeStyle = "rgba(255, 0, 0, 0.8)";
-	dctx.moveTo(10, 10);
-	var j = 0;
-	for (var i=0; i<dRTHistory.length; i++){
-		dctx.moveTo(10+i, 50);
-		dctx.lineTo(10+i, 50-(1000/dRTHistory[i])/2);
-		j += dRTHistory[i];
-	}
-	dctx.stroke();
-	dctx.fillText(Math.round(1000/(j/dRTHistory.length)) + "FPS", 20, 40);
-
-	//Draw gametime chart
-	dctx.beginPath();
-	dctx.strokeStyle = "rgba(128, 0, 128, 0.8)";
-	dctx.moveTo(10, 60);
-	j = 0;
-	for (var i=0; i<dtHistory.length; i++){
-		dctx.moveTo(10+i, 100);
-		dctx.lineTo(10+i, 100-dtHistory[i]/2);
-		j += dtHistory[i];
-	}
-	dctx.stroke();
-	dctx.fillText(Math.round(j/dtHistory.length) + "dt", 20, 90);
-
-	//Draw timeline
-	dctx.beginPath(); //Current Time Marker
-	dctx.strokeStyle = "rgba(0, 128, 128, 0.8)";
-	dctx.moveTo(2, 110);
-	dctx.lineTo(50, 110);
-	dctx.stroke();
-	dctx.fillText(currentGameTime + "ms", 70, 110);
-
-	dctx.beginPath(); //Second ticks
-	dctx.strokeStyle = "rgba(0, 0, 0, 0.8)";
-	dctx.moveTo(10, 110);
-	dctx.lineTo(10, 410);
-	for (var i=0; i<29; i++){
-		var y = 120 + (10 * i) - (currentGameTime % 1000)/100;
-		dctx.moveTo(5, y);
-		dctx.lineTo(15, y);
-	}
-	dctx.stroke();
-
-	dctx.beginPath(); //Event ticks
-	dctx.strokeStyle = "rgba(0, 0, 255, 0.8)";
-	for (var i=0; i<page.game.timeLine.length; i++){
-		y = 110 + (page.game.timeLine[i].time-currentGameTime)/100;
-		dctx.moveTo(2, y);
-		dctx.lineTo(25, y);
-		dctx.fillStyle = "rgba(0, 0, 0, 0.2)";
-		dctx.fillText(page.game.timeLine[i].desc + ": " + page.game.timeLine[i].para, 25, y);
-	}
-	dctx.stroke();
-
-	//Draw ball
-	ctx.beginPath();
-	ctx.arc(page.game.ball.getXPos(), page.game.ball.getYPos(), page.game.ball.radius, 0, Math.PI*2, true);
-	ctx.fillStyle = "rgba(0, 128, 175, 0.8)";
-	ctx.fill();
-
-	//Draw physics test thing
-	ctx.beginPath();
-	ctx.arc(page.game.test.x + 200, page.game.test.y + 200, 10, 0, Math.PI*2, true);
-	ctx.fillStyle = "rgba(0, 175, 64, 0.8)";
-	ctx.fill();
-
-	//Draw dragon
-	ctx.beginPath();
-	ctx.arc(page.game.dragon.getXPos(), page.game.dragon.getYPos(), page.game.dragon.radius, 0, Math.PI*2, true);
-	ctx.fillStyle = "rgba(0, 0, 175, 0.8)";
-	ctx.fill();
-
-	//draw grid
-	ctx.lineWidth = 1;
-	ctx.beginPath();
-	ctx.strokeStyle = "rgba(0, 0, 0, 0.1)";
-	for(var x = 0; x < 600; x = x + 10){
-		ctx.moveTo(x, 0);
-		ctx.lineTo(x, 400);
-	}
-	for(var y = 10; y < 400; y = y + 10){
-		ctx.moveTo(0, y);
-		ctx.lineTo(600, y);
-	}
-	ctx.stroke();
-}
-
-// This is the initialization function
-(function() {
-	window.onload = function(){
+	// This is the initialization function
+	(function() {
 		//debugger;
 		page.canvas = document.getElementById("canvas");
 		page.dPanel = document.getElementById("dPanel");
@@ -263,10 +264,10 @@ function draw(){
 
 		//Game objects
 		var game = page.game;
-		game.ball = new window.Particle(10, 10, 1.0);
+		game.ball = new Particle(10, 10, 1.0);
 		game.ball.setPos(100, 50);
 		game.ball.setVel(0.1, 0.1);
-		game.dragon = new window.Particle(20, 20, 0.999);
+		game.dragon = new Particle(20, 20, 0.999);
 		game.dragon.setPos(300, 100);
 		game.test = {x: 80, y: 0, vx: 0, vy: 0};
 
@@ -286,5 +287,5 @@ function draw(){
 
 		//That's enough foreplay - let's get this show on the road!  First frame, here we come...
 		nextFrame();
-	};
-})();
+	})();
+});
--- app/physics.js
+++ app/physics.js
@@ -1,93 +1,87 @@
 "use strict";
+define({
+    // Returns the linear interpolation between a and b
+    lerp: function(a, b, factor) {
+        return a + factor * (b - a);
+    },
 
-// Put physics related stuff into it's own namespace
-var physics = {};
+    // A store of all state related to the physics engine
+    context: {
+        objects: [],
+        gravity: {x: 0, y: 10},
+    },
 
-// Returns the linear interpolation between a and b
-physics.lerp = function(a, b, factor)
-{
-    return a + factor * (b - a);
-};
+    Derivative: function() {
+        this.dx = 0;
+        this.dy = 0;
+        this.dvx = 0;
+        this.dvy = 0;
+    },
 
-// A store of all state related to the physics engine
-physics.context = {
-    objects: [],
-    gravity: {x: 0, y: 10},
-};
+    // Simple spring calculation
+    acceleration: function(state, t) {
+        var k = 0.3; // Spring constant
+        var b = 0.1; // Spring Damping
+        var l = 80; // Spring Rest length
 
-physics.Derivative = function() {
-    this.dx = 0;
-    this.dy = 0;
-    this.dvx = 0;
-    this.dvy = 0;
-};
+        // Calculate the unit vector of the spring direction
+        var length = Math.sqrt(state.x * state.x + state.y * state.y);
+        var ux = state.x / length;
+        var uy = state.y / length;
 
-// Simple spring calculation
-physics.acceleration = function(state, t)
-{
-    var k = 0.3; // Spring constant
-    var b = 0.1; // Spring Damping
-    var l = 80; // Spring Rest length
+        // Calculate the force made by the spring
+        var fx = -k * (length-l) * ux;
+        var fy = -k * (length-l) * uy;
 
-    // Calculate the unit vector of the spring direction
-    var length = Math.sqrt(state.x * state.x + state.y * state.y);
-    var ux = state.x / length;
-    var uy = state.y / length;
+        // Add the dampening force of the spring
+        var dot = state.vx*ux + state.vy*uy;
+        fx += -ux * dot * b;
+        fy += -uy * dot * b;
 
-    // Calculate the force made by the spring
-    var fx = -k * (length-l) * ux;
-    var fy = -k * (length-l) * uy;
+        var mass = 1;
 
-    // Add the dampening force of the spring
-    var dot = state.vx*ux + state.vy*uy;
-    fx += -ux * dot * b;
-    fy += -uy * dot * b;
+        // Add gravity
+        fx += this.context.gravity.x * mass;
+        fy += this.context.gravity.y * mass;
 
-    var mass = 1;
+        // Divide force with mass to get acceleration
+        return {x: fx / mass, y: fy / mass};
+    },
 
-    // Add gravity
-    fx += physics.context.gravity.x * mass;
-    fy += physics.context.gravity.y * mass;
+    evaluate: function(initial, t, dt, derivative) {
+        var state = {
+            x: initial.x + derivative.dx*dt,
+            y: initial.y + derivative.dy*dt,
+            vx: initial.vx + derivative.dvx*dt,
+            vy: initial.vy + derivative.dvy*dt,
+        };
 
-    // Divide force with mass to get acceleration
-    return {x: fx / mass, y: fy / mass};
-};
+        var acc = this.acceleration(state, t+dt);
 
-physics.evaluate = function(initial, t, dt, derivative)
-{
-    var state = {
-        x: initial.x + derivative.dx*dt,
-        y: initial.y + derivative.dy*dt,
-        vx: initial.vx + derivative.dvx*dt,
-        vy: initial.vy + derivative.dvy*dt,
-    };
+        return {
+            dx: state.vx,
+            dy: state.vy,
+            dvx: acc.x,
+            dvy: acc.y,
+        };
+    },
 
-    var acc = physics.acceleration(state, t+dt);
+    // An implementation of Runge Kutta order 4 integrator
+    // see http://gafferongames.com/game-physics/integration-basics/
+    integrate: function(state, t, dt) {
+        var a = this.evaluate(state, t, 0, new this.Derivative());
+        var b = this.evaluate(state, t, dt*0.5, a);
+        var c = this.evaluate(state, t, dt*0.5, b);
+        var d = this.evaluate(state, t, dt, c);
 
-    return {
-        dx: state.vx,
-        dy: state.vy,
-        dvx: acc.x,
-        dvy: acc.y,
-    };
-};
+        var dxdt = 1 / 6 * (a.dx + 2*(b.dx + c.dx) + d.dx);
+        var dydt = 1 / 6 * (a.dy + 2*(b.dy + c.dy) + d.dy);
+        var dvxdt = 1 / 6 * (a.dvx + 2*(b.dvx + c.dvx) + d.dvx);
+        var dvydt = 1 / 6 * (a.dvy + 2*(b.dvy + c.dvy) + d.dvy);
 
-// An implementation of Runge Kutta order 4 integrator
-// see http://gafferongames.com/game-physics/integration-basics/
-physics.integrate = function(state, t, dt)
-{
-    var a = physics.evaluate(state, t, 0, new physics.Derivative());
-    var b = physics.evaluate(state, t, dt*0.5, a);
-    var c = physics.evaluate(state, t, dt*0.5, b);
-    var d = physics.evaluate(state, t, dt, c);
-
-    var dxdt = 1 / 6 * (a.dx + 2*(b.dx + c.dx) + d.dx);
-    var dydt = 1 / 6 * (a.dy + 2*(b.dy + c.dy) + d.dy);
-    var dvxdt = 1 / 6 * (a.dvx + 2*(b.dvx + c.dvx) + d.dvx);
-    var dvydt = 1 / 6 * (a.dvy + 2*(b.dvy + c.dvy) + d.dvy);
-
-    state.x = state.x + dxdt * dt;
-    state.y = state.y + dydt * dt;
-    state.vx = state.vx + dvxdt * dt;
-    state.vy = state.vy + dvydt * dt;
-};
+        state.x = state.x + dxdt * dt;
+        state.y = state.y + dydt * dt;
+        state.vx = state.vx + dvxdt * dt;
+        state.vy = state.vy + dvydt * dt;
+    },
+});
